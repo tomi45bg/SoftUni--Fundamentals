aa08396f191cbb0ea64e1981d2b4a7e87374f94a
tomi45bg <omftomas@gmail.com>
2023-05-29 19:38:48 +0300

    done!!!

--- Arrays - Exercise/07. Max Sequence of Equal Elements/07. Max Sequence of Equal Elements.cs
+++ Arrays - Exercise/07. Max Sequence of Equal Elements/07. Max Sequence of Equal Elements.cs
@@ -1,10 +1,82 @@
-﻿namespace _07._Max_Sequence_of_Equal_Elements
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int largestCount = 1;
+            int numberToPrint = 0;
+            int[] indexes = new int[array.Length];
+            int[] numbers = new int[array.Length];
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; i < array.Length; j++)
+                {
+                    if (j == array.Length)
+                    {
+                        if (largestCount == 1)
+                        {
+                            largestCount = 1;
+                            numberToPrint = array[i];
+                        }
+                        indexes[count] = largestCount;
+                        numbers[count] = numberToPrint;
+                        count++;
+
+                        i = j - 1;
+
+                        largestCount = 1;
+                        numberToPrint = 0;
+                        break;
+                    }
+
+                    if (array[i] == array[j])
+                    {
+                        largestCount++;
+                        numberToPrint = array[i];
+                    } else
+                    {
+                        if (largestCount == 1)
+                        {
+                            largestCount = 1;
+                            numberToPrint = array[i];
+                        }
+                        indexes[count] = largestCount;
+                        numbers[count] = numberToPrint;
+                        count++;
+
+                        i = j - 1;
+
+                        largestCount = 1;
+                        numberToPrint = 0;
+
+                        break;
+                    }
+                }
+            }
+
+            int countToPrint = 1;
+            int maxNumber = 0;
+
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                if (indexes[i] > countToPrint)
+                {
+                    countToPrint = indexes[i];
+                    maxNumber = numbers[i];
+                }
+            }
+
+            for (int i = 0; i < countToPrint; i++)
+            {
+                Console.Write(maxNumber + " ");
+            }
         }
     }
 }
