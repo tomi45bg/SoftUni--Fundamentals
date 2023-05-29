f52dc7081b93ec53b129869bf76ff72ebed13c8a
tomi45bg <omftomas@gmail.com>
2023-05-29 20:06:24 +0300

    done

---
+++ Arrays - Exercise/08. Magic Sum/08. Magic Sum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            int[] firstNum = new int[array.Length];
+            int[] secondNum = new int[array.Length];
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == num)
+                    {
+                        firstNum[count] = array[i];
+                        secondNum[count] = array[j];
+                        count++;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(firstNum[i] + " " + secondNum[i]);
+            }
+        }
+    }
+}
