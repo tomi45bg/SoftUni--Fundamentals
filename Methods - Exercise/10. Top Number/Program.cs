03c8ae2355e2535e6a8ef7f4b22f8fd1fa27ecd1
tomi45bg <omftomas@gmail.com>
2023-06-04 16:24:32 +0300

    done

--- Methods - Exercise/10. Top Number/Program.cs
+++ Methods - Exercise/10. Top Number/Program.cs
@@ -7,7 +7,36 @@ namespace _10._Top_Number
     {
         static void topNumbers(int number)
         {
+            for (int i = 1; i <= number; i++)
+            {
+                int num = i;
+                bool isDivisible8 = false;
+                bool hasEven = false;
+                int sum = 0;
 
+                while (num > 0)
+                {
+                    sum += num % 10;
+
+                    if ((num % 10) % 2 != 0)
+                    {
+                        hasEven = true;
+                    }
+
+                    num /= 10;
+                }
+
+                if (sum % 8 == 0)
+                {
+                    isDivisible8 = true;
+                }
+
+                if (hasEven && isDivisible8)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
 
         static void Main(string[] args)
