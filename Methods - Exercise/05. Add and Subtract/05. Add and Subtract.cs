481eb5c35aeb58ec02ebbfc3a408c9cc06d4cdef
tomi45bg <omftomas@gmail.com>
2023-06-04 15:48:20 +0300

    done

---
+++ Methods - Exercise/05. Add and Subtract/05. Add and Subtract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int num4 = sum(num1, num2);
+            int num5 = subtract(num4, num3);
+
+            Console.WriteLine(num5);
+        }
+    }
+}
