7767722b2230ac7e9b90e831e67e33ee8648dcb2
tomi45bg <omftomas@gmail.com>
2023-06-04 16:18:48 +0300

    done

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static bool isItPalidrome(int num)
+        {
+            bool result = true;
+            string toNum = num.ToString();
+            int count = 1;
+
+            for (int i = 0; i < toNum.Length / 2; i++)
+            {
+                if (toNum[i] != toNum[toNum.Length - count])
+                {
+                    result = false;
+                    break;
+                }
+
+                count++;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while(command != "END")
+            {
+                int number = int.Parse(command);
+                bool isPalidrome = isItPalidrome(number);
+
+                if (isPalidrome)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Methods - Exercise/10. Top Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void topNumbers(int number)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            int endNumber = int.Parse(Console.ReadLine());
+
+            topNumbers(endNumber);
+        }
+    }
+}
