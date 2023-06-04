adedeba0cf4ee2a700e5f036a14de30e685904a3
tomi45bg <omftomas@gmail.com>
2023-06-04 15:54:14 +0300

    done

---
+++ Methods - Exercise/06. Middle Characters/06. Middle Characters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static string findMiddle(string text)
+        {
+            string middle = "";
+
+            if (text.Length % 2 != 0)
+            {
+                middle += text[text.Length / 2];
+            } else
+            {
+                middle += text[text.Length / 2 - 1];
+                middle += text[text.Length / 2 + 1];
+            }
+
+            return middle;
+        }
+
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string middlePart = findMiddle(word);
+            Console.WriteLine(middlePart);
+        }
+    }
+}
