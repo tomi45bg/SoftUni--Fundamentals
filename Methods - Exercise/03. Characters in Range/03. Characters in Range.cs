cb9d61f4f2f8bd93f79f6b34a19be642bba2bbe5
tomi45bg <omftomas@gmail.com>
2023-06-04 15:06:27 +0300

    done

---
+++ Methods - Exercise/03. Characters in Range/03. Characters in Range.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void betweenCharacters(char first, char second)
+        {
+            if ( (int) first <= (int)second)
+            {
+                for (int i = first + 1; i < second; i++)
+                {
+                    Console.Write((char) i + " ");
+                }
+            } else
+            {
+                for (int i = second + 1; i < first; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            betweenCharacters(firstChar, secondChar);
+        }
+    }
+}
