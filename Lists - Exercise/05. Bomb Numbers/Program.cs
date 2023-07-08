dba4db01c4dc387a3b93b68dcb33be7066a4c1b3
tomi45bg <omftomas@gmail.com>
2023-07-08 15:14:23 +0300

    done

---
+++ Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static List<int> detonations(List<int> numbers, int number, int power)
+        {
+            int indexBomb = 0, leftDiff = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    indexBomb = i;
+                    break;
+                }
+            }
+            int index = indexBomb;
+            int indexA = indexBomb;
+
+            //left
+            for (int i = 1; i <= power; i++)
+            {
+                if (indexBomb - 1 < 0)
+                {
+                    break;
+                } else
+                {
+                    numbers.RemoveAt(indexBomb - 1);
+                    leftDiff++;
+                    indexBomb--;
+                }
+            }
+
+
+            //right
+            for (int i = 1; i <= power; i++)
+            {
+                if (index - power + 1 > numbers.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    numbers.RemoveAt(index - power + 1);
+                    index++;
+                }
+            }
+
+            numbers.RemoveAt(indexA - leftDiff);
+
+            return numbers;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> bomb = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int specialNumber = bomb[0];
+            int power = bomb[1];
+
+            numbers = detonations(numbers, specialNumber, power);
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace _01._Advertisement_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Phrases – {"Excellent product.", "Such a great product.", "I always use that
+                product.", "Best product of its category.", "Exceptional product.", "I can't
+                    live without this product."}
+•                Events – {"Now I feel good.", "I have succeeded with this product.", "Makes
+                    miracles. I am happy of the results!", "I cannot believe but now I feel
+                    awesome.", "Try it yourself, I am very satisfied.", "I feel great!"}
+•               Authors – {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"}
+•               Cities – {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"}
+
+             * 
+             */
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string phrase = "";
+                string events = "";
+                string author = "";
+                string city = "";
+
+                string[] phrases = {"Excellent product.",
+                    "Such a great product.", "I always use that product.",
+                    "Best product of its category.", "Exceptional product.",
+                    "I can't live without this product."};
+
+                string[] eventsAll = {"Now I feel good.",
+                    "I have succeeded with this product.",
+                    "Makes miracles. I am happy of the results!",
+                    "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.",
+                    "I feel great!" };
+
+                string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+                string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+                Random random = new Random();
+
+                phrase = phrases[random.Next(phrases.Length)];
+                events = eventsAll[random.Next(eventsAll.Length)];
+                author = authors[random.Next(authors.Length)];
+                city = cities[random.Next(cities.Length)];
+
+                Console.WriteLine($"{phrase} {events} {author} – {city}.");
+            }
+        }
+    }
+}
